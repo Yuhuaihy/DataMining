780f1d609a5d83c5530f1c23da2fc6a3baaad8dc
Yuhuaihy <yhuai@terpmail.umd.edu>
2018-10-21 23:52:14 -0400

    .

--- homework2/p6.py
+++ homework2/p6.py
@@ -1,1 +1,10 @@
 ###SVM
+from svm import SVM
+import numpy as np
+trainX_path = 'datasets/train_data.mat'
+trainy_path = 'datasets/train_label.mat'
+testX_path = 'datasets/test_data.mat'
+testy_path = 'datasets/test_label.mat'
+trainX = 
+
+svm = SVM()
