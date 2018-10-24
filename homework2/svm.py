600d0d9dbbfe438b8eda54a2ac6e33b21dd1ed0e
Yuhuaihy <yhuai@terpmail.umd.edu>
2018-10-24 18:19:13 -0400

    .

--- homework2/svm.py
+++ homework2/svm.py
@@ -8,7 +8,9 @@ class SVM(object):
         bias = np.ones((1,self.m))
         self.Xtrain = np.insert(Xtrain, 0, values=bias, axis=1)
         _, self.n = self.Xtrain.shape
-        self.Xtest = np.insert(Xtest, 0, values=bias, axis=1)
+        self.m2, _ = ytest.shape
+        bias2 = np.ones((1,self.m2))
+        self.Xtest = np.insert(Xtest, 0, values=bias2, axis=1)
         self.ytest = ytest
         
         
@@ -16,20 +18,24 @@ class SVM(object):
     def fit(self):
         w = np.zeros((self.classes, self.n))
         eta = 1
-        epochs = 100
+        epochs = 1000
         for epoch in range(1,epochs):
             for i in range(self.m):
                 for j in range(self.classes):
                     y = self.ytrain[i][j]
-                    if y * np.dot(self.Xtrain[i],w[j]) < 1:
+                    val = np.dot(self.Xtrain[i],w[j])
+                    if y * val < 1:
                         w[j] += eta * (self.Xtrain[i] * y) + (-2 * (1/epochs) * w[j])
                     else:
                         w[j] = w[j] + eta * (-2  *(1/epoch)* w[j])
         self.w = w
         
     
-    def predict(self, Xtest, ytest):
-        predy = np.dot(X, self.w.transpose())
+    def predict(self):
+
+        self.predy = np.dot(self.Xtest, self.w.transpose()) ### 10000*10
+        self.label = np.where(self.predy>1)
+        test_label = np.where(self.ytest == 1)[1].reshape((self.m2,1))
         embed()
     
     def plot(self):
